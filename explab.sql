37f7e51438d68770920c9efe640b670ff8d34e97
habibrmns <74051423+habibrmns@users.noreply.github.com>
2020-11-06 17:30:18 +0530

    Add files via upload

---
+++ explab.sql
@@ -0,0 +1,22 @@
+create table student(sno int,name varchar(20),marks int,dept char(25));
+
+
+alter table student add column age int;
+
+
+alter table student modify column dept char(20);
+
+
+alter table student drop column marks;
+
+
+alter table student rename to students;
+
+
+insert into students values(27, 'habeeb','cse',20);
+
+
+truncate students;
+
+
+drop table students;
